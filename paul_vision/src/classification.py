4617009ba00580593dd3d2a2f267bc5cf6af390e
Victoria <victoria.pitz.clairoux@usherbrooke.ca>
2022-04-23 17:58:58 -0400

    refactoring

--- paul_vision/src/classification.py
+++ paul_vision/src/classification.py
@@ -11,10 +11,6 @@ import os
 bridge = CvBridge()
 import rospkg
 
-# dictionnary of objets
-# bbox coordinates
-# name of object with most match
-# number of matches
 
 class find_item:
     def __init__(self):
@@ -143,47 +139,47 @@ if __name__ == '__main__':
 
 
 
-            # detector = cv2.ORB_create()
+# detector = cv2.ORB_create()
 
-            # # find the keypoints and descriptors with ORB
-            # kp1, des1 = detector.detectAndCompute(img1,None)
-            # kp2, des2 = detector.detectAndCompute(img2,None)
-            
-            # # create BFMatcher object
-            # bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-            # # Match descriptors.
-            # matches = bf.match(des1,des2)
-            # # Sort them in the order of their distance.
-            # matches = sorted(matches, key = lambda x:x.distance)
+# # find the keypoints and descriptors with ORB
+# kp1, des1 = detector.detectAndCompute(img1,None)
+# kp2, des2 = detector.detectAndCompute(img2,None)
 
-            # good_matches = matches[:20]
+# # create BFMatcher object
+# bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+# # Match descriptors.
+# matches = bf.match(des1,des2)
+# # Sort them in the order of their distance.
+# matches = sorted(matches, key = lambda x:x.distance)
 
-            # src_pts = np.float32([ kp1[m.queryIdx].pt for m in good_matches     ]).reshape(-1,1,2)
-            # dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good_matches ]).reshape(-1,1,2)
-            # M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,3.0)
-            # matchesMask = mask.ravel().tolist()
-            # h,w = img1.shape[:2]
-            # pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+# good_matches = matches[:20]
 
-            # dst = cv2.perspectiveTransform(pts,M)
-            # dst += (w, 0)  # adding offset
+# src_pts = np.float32([ kp1[m.queryIdx].pt for m in good_matches     ]).reshape(-1,1,2)
+# dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good_matches ]).reshape(-1,1,2)
+# M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,3.0)
+# matchesMask = mask.ravel().tolist()
+# h,w = img1.shape[:2]
+# pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
 
-            # draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-            #             singlePointColor = None,
-            #             matchesMask = matchesMask, # draw only inliers
-            #             flags = 2)
+# dst = cv2.perspectiveTransform(pts,M)
+# dst += (w, 0)  # adding offset
 
-            # img3 = cv2.drawMatches(img1,kp1,img2,kp2,good_matches, None,**draw_params)
-            # img3 = cv2.polylines(img3, [np.int32(dst)], True, (0,0,255),3, cv2.LINE_AA)
+# draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+#             singlePointColor = None,
+#             matchesMask = matchesMask, # draw only inliers
+#             flags = 2)
+
+# img3 = cv2.drawMatches(img1,kp1,img2,kp2,good_matches, None,**draw_params)
+# img3 = cv2.polylines(img3, [np.int32(dst)], True, (0,0,255),3, cv2.LINE_AA)
 
 
-            # BFMatcher with default params
-            # bf = cv2.BFMatcher()
-            # matches = bf.knnMatch(des1,des2,k=2)
-            # # Apply ratio test
-            # good_matches = []
-            # for m,n in matches:
-            #     if m.distance < 0.75*n.distance:
-            #         good_matches.append([m])
+# BFMatcher with default params
+# bf = cv2.BFMatcher()
+# matches = bf.knnMatch(des1,des2,k=2)
+# # Apply ratio test
+# good_matches = []
+# for m,n in matches:
+#     if m.distance < 0.75*n.distance:
+#         good_matches.append([m])
 
-            # img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good_matches,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+# img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good_matches,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
